19c3fa20cb1323abe65cb7d2702b95fba03c8ed3
Núria Mercadé-Besora <61558739+nmercadeb@users.noreply.github.com>
2024-07-31 10:39:49 +0100

    cc from cran

--- CohortConstructor/major_non_cardiac_surgery.R
+++
@@ -1,8 +0,0 @@
-cdm$cc1_major_non_cardiac_surgery <- conceptCohort(
-  cdm = cdm,
-  conceptSet = codes[c("major_non_cardiac_surgery")],
-  name = "cc1_major_non_cardiac_surgery"
-) |>
-  requireIsFirstEntry() |>
-  requireAge(ageRange = list(c(18, 150))) |>
-  exitAtObservationEnd()
